ab1eac02795dc63fb2109aa24ad47902db0738ff
Andrey Guliy <iam@agoose.ru>
2026-03-09 12:14:51 +0200

    Initial Commit

---
+++ MyMessanger/MyMessangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMessangerApp.swift
+//  MyMessanger
+//
+//  Created by Гулий Андрей on 09.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct MyMessangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
